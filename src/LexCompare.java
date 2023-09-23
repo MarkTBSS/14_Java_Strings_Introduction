992c92e91cc2886e44120aecb310d70b33505a70
MarkTBSS <marktbss@gmail.com>
2023-09-23 09:23:57 +0700

    [ADD] Java Files

---
+++ src/LexCompare.java
@@ -0,0 +1,13 @@
+public class LexCompare {
+    public static String compareStrings(String a, String b) {
+        int result = a.compareTo(b);
+
+        if (result <= 0) {
+            return "No";
+        } else if (result > 0) {
+            return "Yes";
+        } else {
+            return "Equal";
+        }
+    }
+}
---
+++ src/Solution.java
@@ -0,0 +1,28 @@
+//import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        String mockInput = "hello\n" +
+                        "java";
+        Scanner scanner = new Scanner(mockInput);
+        String a = scanner.next();
+        String b = scanner.next();
+        System.out.println(a.length() + b.length());
+        scanner.close();
+
+        String compareResult = LexCompare.compareStrings(a, b);
+        System.out.println(compareResult);
+
+        String aUpper = a.substring(0, 1).toUpperCase()
+        + a.substring(1);
+        System.out.println(aUpper);
+
+        String bUpper = b.substring(0, 1).toUpperCase()
+        + b.substring(1);
+        System.out.println(bUpper);
+
+        String aPlusB = aUpper + " " + bUpper;
+        System.out.println(aPlusB);
+    }
+}
