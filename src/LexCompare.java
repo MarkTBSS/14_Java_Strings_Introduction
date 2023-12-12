e8990c669804ad4b854c28791b27dbe5ba1e9d5b
MarkTBSS <marktbss@gmail.com>
2023-12-12 09:20:05 +0700

    [MOD] Delete .class

--- src/LexCompare.java
+++ src/LexCompare.java
@@ -1,12 +1,15 @@
 public class LexCompare {
     public static String compareStrings(String a, String b) {
         int result = a.compareTo(b);
+        String resultString = "Equal";
         if (result <= 0) {
-            return "No";
-        } else if (result > 0) {
-            return "Yes";
-        } else {
-            return "Equal";
-        }
+            resultString = "No";
+            return resultString;
+        } 
+        if (result > 0) {
+            resultString = "Yes";
+            return resultString;
+        } 
+        return resultString;
     }
 }
